c7867fa0fd8849c6d8ca5416a1fb90e600262810
sanjay_b <sanjay_b@persistent.com>
2019-05-28 10:48:06 +0530

    add more test cases

--- test/TicTacToe.test.js
+++ test/TicTacToe.test.js
@@ -116,10 +116,74 @@ contract("TicTacToe declares winner correctly", accounts => {
       assert(true);
     }
   });
+  it("doesn't let the looser pull money", async () => {
+    try {
+      await game.claimRefund({ from: O });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
   it("lets the winner pull money", async () => {
     const initialBalance = await web3.eth.getBalance(X);
     await game.claimRefund({ from: X });
     const finalBalance = await web3.eth.getBalance(X);
+    assert(finalBalance > initialBalance);
+  });
+});
+
+contract("TicTacToe handles a draw correctly", accounts => {
+  let game;
+  let gameAddress;
+  let zeroAddress;
+  const X = accounts[1];
+  const O = accounts[2];
+
+  beforeEach("setup a TicTacToe contract", async () => {
+    const managerInstance = await GameManager.deployed();
+    await managerInstance.createGame(1, 1, { from: X, value: value });
+
+    gameAddress = await managerInstance.lastGame();
+    game = await TicTacToe.at(gameAddress);
+    zeroAddress = await game.winner();
+    await game.join(0, 0, { from: O, value: value });
+    await game.play(2, 2, { from: X });
+    await game.play(0, 2, { from: O });
+    await game.play(0, 1, { from: X });
+    await game.play(2, 1, { from: O });
+    await game.play(1, 0, { from: X });
+    await game.play(1, 2, { from: O });
+    await game.play(2, 0, { from: X });
+  });
+  it("records the draw correctly", async () => {
+    const winner = await game.winner();
+    const XRefund = await game.refunds(X);
+    const ORefund = await game.refunds(O);
+    const balance = await web3.eth.getBalance(gameAddress);
+    const stage = await game.stage();
+
+    assert.strictEqual(zeroAddress, winner);
+    assert.strictEqual(balance, Number(XRefund) + Number(ORefund) + "");
+    assert.strictEqual(3, Number(stage));
+  });
+  it("ends the game after winning", async () => {
+    try {
+      await game.play(2, 2, { from: O });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
+  it("lets the initiator pull money", async () => {
+    const initialBalance = await web3.eth.getBalance(X);
+    await game.claimRefund({ from: X });
+    const finalBalance = await web3.eth.getBalance(X);
+    assert(finalBalance > initialBalance);
+  });
+  it("lets the responder pull money", async () => {
+    const initialBalance = await web3.eth.getBalance(O);
+    await game.claimRefund({ from: O });
+    const finalBalance = await web3.eth.getBalance(O);
     assert(finalBalance > initialBalance);
   });
 });
