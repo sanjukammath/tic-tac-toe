78f585f139e0e0ca99dc01ea8357f3f9be215c49
sanjay_b <sanjay_b@persistent.com>
2019-06-13 15:47:58 +0530

    add more tests for multiple games

--- test/TicTacToe.test.js
+++ test/TicTacToe.test.js
@@ -34,7 +34,15 @@ contract("TicTacToe can add a new game", accounts => {
     });
     it("should check for minimum bounty", async () => {
       try {
-        await gameInstance.createGame(1, 1, { from: accounts[1], value: 1 });
+        await game.createGame(1, 1, { from: accounts[1], value: 1 });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+    it("should check for row and colum bound", async () => {
+      try {
+        await game.createGame(3, 3, { from: accounts[1], value: value });
         assert(false);
       } catch {
         assert(true);
@@ -78,13 +86,29 @@ contract("TicTacToe lets responder join", accounts => {
     });
     it("should not let responder to mark the same box", async () => {
       try {
-        await game.join(index, 1, 1, { from: accounts[2], value: 1000 });
+        await game.join(index, 1, 1, { from: accounts[2], value: value });
         assert(false);
       } catch {
         assert(true);
       }
     });
-
+    it("should not let join a non existent game", async () => {
+      try {
+        await game.join(index + 5, 0, 0, { from: accounts[2], value: value });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+    it("should not let join an already joined game", async () => {
+      await game.join(index, 0, 0, { from: accounts[2], value: value });
+      try {
+        await game.join(index, 2, 2, { from: accounts[2], value: value * 5 });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
     it("should update game properties on join", async () => {
       await game.join(index, 0, 0, { from: accounts[2], value: value });
 
@@ -104,6 +128,89 @@ contract("TicTacToe lets responder join", accounts => {
       assert.strictEqual(false, result);
       assert.strictEqual(1, Number(stage));
     });
+  });
+});
+
+contract("TicTacToe lets players play", accounts => {
+  let game;
+  let numberOfGames;
+  let index;
+  beforeEach("setup a TicTacToe contract", async () => {
+    game = await TicTacToe.deployed();
+
+    await game.createGame(1, 1, { from: accounts[1], value: value });
+
+    numberOfGames = await game.numberOfGames();
+    index = Number(numberOfGames) - 1;
+  });
+  describe("Play Game", () => {
+    it("should not let same player play twice", async () => {
+      await game.join(index, 0, 0, { from: accounts[2], value: value });
+      try {
+        await game.play(index, 0, 1, { from: accounts[2] });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+    it("should not let player to mark the same box", async () => {
+      await game.join(index, 0, 0, { from: accounts[2], value: value });
+      try {
+        await game.play(index, 0, 0, { from: accounts[1] });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+    it("should not let join a non existent game", async () => {
+      try {
+        await game.play(index + 5, 0, 0, { from: accounts[2] });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+    it("should not let different player to play", async () => {
+      await game.join(index, 0, 0, { from: accounts[2], value: value });
+      await game.play(index, 0, 1, { from: accounts[1] });
+      try {
+        await game.play(index, 0, 2, { from: accounts[1] });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+    it("should not let player to play without joining", async () => {
+      try {
+        await game.play(index, 0, 1, { from: accounts[2] });
+        assert(false);
+      } catch {
+        assert(true);
+      }
+    });
+  });
+});
+
+contract("TicTacToe lets players claim tokens and refund", accounts => {
+  let game;
+  beforeEach("setup a TicTacToe contract", async () => {
+    game = await TicTacToe.deployed();
+  });
+  it("doesn't allow player to withdraw token without valid balance", async () => {
+    try {
+      await game.claimTokens({ from: accounts[1] });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
+  it("doesn't allow player to claim ether without giving tokens", async () => {
+    try {
+      await game.claimRefund({ from: accounts[1] });
+      assert(false);
+    } catch {
+      assert(true);
+    }
   });
 });
 
@@ -170,7 +277,7 @@ contract("TicTacToe declares winner correctly", accounts => {
     await game.claimRefund({ from: X });
     const finalBalance = await web3.eth.getBalance(X);
 
-    assert(finalBalance > initialBalance);
+    assert(Number(finalBalance) > Number(initialBalance));
   });
 });
 
