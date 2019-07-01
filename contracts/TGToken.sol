1d68eae79d4a5ebc5666fc66cd799aaf8a732c2d
sanjay_b <sanjay_b@persistent.com>
2019-07-01 14:16:59 +0530

    initial refactor of the smart contracts

--- contracts/TGToken.sol
+++ contracts/TGToken.sol
@@ -1,9 +1,95 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract TGToken is ERC20Mintable {
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract TGToken is ERC20Interface {
+    using SafeMath for uint256;
     string public name = "TicTacToeGameToken";
     string public symbol = "TGT";
     uint8 public decimals = 2;
+    uint public initialSupply = 10000;
+    address public contractOwner;
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    
+    constructor() public {
+        contractOwner = msg.sender;
+        _balances[msg.sender] = initialSupply;
+    }
+    
+    function requestTokens() public {
+        require(_balances[msg.sender] < 5, "Sender has enough tokens");
+        require(_balances[contractOwner] > 5, "All current tokens have been distributed");
+        
+        _balances[contractOwner] = _balances[contractOwner].sub(5);
+        _balances[msg.sender] = _balances[msg.sender].add(5);
+        emit Transfer(contractOwner, msg.sender, 5);
+    }
+    
+    function mint(uint amount) public {
+        require (msg.sender == contractOwner, "Only token manager can mint");
+        _balances[contractOwner] = _balances[contractOwner].add(amount);
+        _totalSupply = _totalSupply.add(amount);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
 }
--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,178 +1,226 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
 contract TicTacToe {
-    event Joined (uint id, address responder);
-    event Played (uint id, address player);
-    event Over (uint id, string result);
+    using SafeMath for uint256;
     
-    enum Stages { Created, Joined, Over }
-    
-    struct Game{
-        address payable X;
-        address payable O;
-        mapping(uint8 => mapping(uint8 => uint8)) board;
-        address lastTurn;
-        Stages stage;
-        uint bounty;
-        uint8 numberOfTurns;
-        bool result;
-    }
-    
-    mapping(address => uint) public tokenBalance;
-    mapping(uint => Game) public games;
+    event Accepted (uint id, address responder);
+    event Started (uint id, address player);
+    event Saved (uint id, address player, int8[9] board);
+    event Closed (uint id, string result, address winner);
     
     address public tokenAddress;
     uint public numberOfGames;
+    mapping(address => uint) public refund;
+    
+    enum BidStates {Created, Accepted, Started, Disbursed}
 
-    constructor (address a) public{
+    struct Bid{
+        address bidder;
+        uint value;
+        address acceptor;
+        BidStates state;
+        uint startDate;
+        uint bidTimeOut;
+    }
+    
+    mapping(uint => Bid) public bids;
+    
+    enum GameStates {Started, Closed, Drew , Won}
+    
+    
+    struct Game{
+        address X;
+        address O;
+        mapping(uint8 => mapping(uint8 => int8)) board;
+        GameStates state;
+        uint bidId;
+        uint8 numberOfTurns;
+        address winner;
+    }
+    
+    mapping(uint => Game) public games;
+
+    constructor (address a) public {
         tokenAddress = a;
         numberOfGames = 0;
     }
+
+    function bid (uint stake, uint timeOut) public {
+        uint256 approvedTokens = ERC20Interface(tokenAddress).allowance(msg.sender, address(this));
+        require(stake < approvedTokens, "contract not allowed to spend stake amount");
+        require(ERC20Interface(tokenAddress).transferFrom(msg.sender, address(this), stake), "transfer of tokens failed");
+        
+        Bid memory newBid = Bid({
+            bidder: msg.sender,
+            value: stake,
+            acceptor: address(0),
+            state: BidStates.Created,
+            startDate: now,
+            bidTimeOut: timeOut
+        });
+        
+        bids[numberOfGames] = newBid;
+        numberOfGames.add(1);
+    }
     
-    function createGame (uint8 row, uint8 col) public payable {
-        require(msg.value>.001 ether, "Add minimum stake value");
+    function accept(uint id) public{
+        Bid storage currentBid = bids[id];
+        uint value = currentBid.value;
+        require(currentBid.state == BidStates.Created, "This bid is not available for accepting");
+        uint256 approvedTokens = ERC20Interface(tokenAddress).allowance(msg.sender, address(this));
+        require(value < approvedTokens, "contract not allowed to spend stake amount");
+        require(ERC20Interface(tokenAddress).transferFrom(msg.sender, address(this), value), "transfer of tokens failed");
+        
+        currentBid.acceptor = msg.sender;
+        currentBid.state = BidStates.Accepted;
+        currentBid.value = value.add(value);
+        
+        emit Accepted(id, msg.sender);
+    }
+    
+    function start(uint id, uint8 row, uint8 col) public{
+        Bid storage currentBid = bids[id];
+        require(currentBid.state == BidStates.Accepted, "wait for the bid to be acceted before starting a game");
+        require(msg.sender == currentBid.bidder, "only the bidder can make the first move");
         require(row < 3 && col < 3, "index out of bound");
+        
+        uint8 turns = games[id].numberOfTurns;
+        require(turns == 0, "the game has already been started once");
         
         Game memory newGame = Game({
            X: msg.sender,
            O: address(0),
-           lastTurn: msg.sender,
-           stage: Stages.Created,
-           bounty: msg.value,
-           numberOfTurns: 1,
-           result: false
+           state: GameStates.Started,
+           bidId: id,
+           numberOfTurns: 1, 
+           winner: address(0)
         });
-        
-        games[numberOfGames] = newGame;
-        Game storage currentGame = games[numberOfGames];
-        numberOfGames++;
+        games[id] = newGame;
+        Game storage currentGame = games[id];
         currentGame.board[row][col] = 1;
-
+        
+        currentBid.state = BidStates.Started;
+        emit Started(id, msg.sender);
     }
     
-    function join(uint g, uint8 row, uint8 col) public payable{
-        require(g < numberOfGames, "Game not yet created");
+    function save(uint id, int8[9] memory board,  uint8 v, bytes32 r, bytes32 s, uint8 nonce, bool close) public{
+        Game storage currentGame = games[id];
+        require(currentGame.state == GameStates.Started, "There is nothing to save");
+        require(nonce < 10, "invalid nonce");
+        require(currentGame.numberOfTurns < nonce, "Trying to save older state");
+        require(msg.sender == currentGame.X || msg.sender == currentGame.O, "Not a player in this game");
         
-        Game storage currentGame = games[g];
+        bytes memory e = abi.encode(address(this), id, board, nonce);
+        bytes32 h = keccak256(e);
         
-        require(msg.value >= currentGame.bounty, "Match the stake");
-        require(msg.sender != currentGame.X, "Playing with self");
-        require(currentGame.stage==Stages.Created, "Stage is not Created");
-        require(currentGame.board[row][col] == 0);
+        address signer = ecrecover(h, v, r, s);
         
-        currentGame.O = msg.sender;
-        currentGame.board[row][col] = 2;
-        currentGame.bounty+= msg.value;
-        currentGame.stage = Stages.Joined;
-        currentGame.lastTurn = msg.sender;
-        currentGame.numberOfTurns++;
-        emit Joined(g, msg.sender);
-    }
-    
-    function play(uint g, uint8 row, uint8 col) public{
-        require(g < numberOfGames, "Game not yet created");
-        
-        Game storage currentGame = games[g];
-        
-        require(currentGame.stage==Stages.Joined, "Stage is not Joined");
-        require(currentGame.board[row][col] == 0);
-        
-        if (currentGame.lastTurn == currentGame.X) {
-            require(msg.sender == currentGame.O, "This is O's turn");
-            currentGame.board[row][col] = 2;
+        if (msg.sender == currentGame.X) {
+            require(signer == currentGame.O, "You need attestation from opponent");
         } else {
-            require(msg.sender == currentGame.X, "This is X's turn");
-            currentGame.board[row][col] = 1;
+            require(signer == currentGame.X, "You need attestation from opponent");
         }
         
+        saveBoard(currentGame, board);
         
-        currentGame.lastTurn = msg.sender;
-        currentGame.numberOfTurns++;
+        currentGame.numberOfTurns = nonce;
         
-        if (currentGame.numberOfTurns > 4) {
-            bool won = checkWinner(currentGame, row, col);
-            if (won) {
-                tokenBalance[msg.sender] += currentGame.bounty;
-                currentGame.stage = Stages.Over;
-                currentGame.result = true;
-                emit Over(g, "Won");
-            } else if (currentGame.numberOfTurns == 9) {
-                uint drawRefund = currentGame.bounty/2;
-                tokenBalance[currentGame.X] += drawRefund;
-                tokenBalance[currentGame.O] += currentGame.bounty - drawRefund;
-                currentGame.stage = Stages.Over;
-                emit Over(g, "Draw");
-            } else {
-                emit Played(g, msg.sender);
-            }
-        }else {
-            emit Played(g, msg.sender);
-        }        
-    }
-    
-    
-    function checkWinner(Game storage currentGame, uint8 row, uint8 col) private view returns (bool won){
+        emit Saved(id, msg.sender, board);
         
-        uint8 c = currentGame.board[row][col];
-        
-        for(uint8 x = 0; x < 3; x++){
-            if(currentGame.board[x][col] != c)
-                break;
-            if(x == 2){
-                return true;
-            }
-        }
-        
-        for(uint8 y = 0; y < 3; y++){
-            if(currentGame.board[row][y] != c)
-                break;
-            if(y == 2){
-                return true;
-            }
-        }
-        
-        if (row != col){
-            if (row + col != 2) {
-                return false;
-            }
-            for(uint8 x = 0; x < 3; x++){
-                if(currentGame.board[x][(2)-x] != c)
-                    break;
-                if(x == 2){
-                    return true;
-                }
-            }
+        if (close){
+            (bool won, address winner) = checkWinner(currentGame);
             
-        } else {
-            for(uint8 x = 0; x < 3; x++){
-                if(currentGame.board[x][x] != c)
-                    break;
-                if(x == 2){
-                    return true;
-                }
+            if (won) {
+                currentGame.winner = winner;
+                currentGame.state = GameStates.Won;
+                
+                Bid storage currentBid = bids[id];
+                currentBid.state = BidStates.Disbursed;
+                
+                ERC20Interface(tokenAddress).transfer(winner, currentBid.value);
+                
+                emit Closed(id, "Game Won", winner);
+            } else if(currentGame.numberOfTurns == 9) {
+                currentGame.state = GameStates.Drew;
+                
+                Bid storage currentBid = bids[id];
+                currentBid.state = BidStates.Disbursed;
+                uint drawRefund = currentBid.value/2;
+                
+                ERC20Interface(tokenAddress).transfer(currentGame.X, drawRefund);
+                ERC20Interface(tokenAddress).transfer(currentGame.O, currentBid.value - drawRefund);
+                
+                emit Closed(id, "Game Draw", address(0));
+            } else {
+                require(false, "Game cannot be closed without a result. If you want to just save send close as false");
             }
         }
+    }
+    
+    function timeOut(uint id) public{
+        Game storage currentGame = games[id];
+        Bid storage currentBid = bids[id];
+        require(msg.sender == currentBid.bidder || msg.sender == currentBid.acceptor, "Not a player in this game");
         
-        return false;
+        require(currentBid.state != BidStates.Disbursed, "Bid has already been paid out");
+        
+        require(currentBid.startDate + currentBid.bidTimeOut > now, "Bid timeOut has not occured yet");
+        
+        BidStates state = currentBid.state;
+        
+        currentGame.state = GameStates.Closed;
+        currentBid.state = BidStates.Disbursed;
+        
+        
+        if (state==BidStates.Created){
+            ERC20Interface(tokenAddress).transfer(currentBid.bidder, currentBid.value);
+        } else{
+            uint closedRefund = currentBid.value/2;
+                
+            ERC20Interface(tokenAddress).transfer(currentBid.bidder, closedRefund);
+            ERC20Interface(tokenAddress).transfer(currentBid.acceptor, currentBid.value - closedRefund);
+        }
+        
     }
     
-    function claimTokens() public {
-        uint senderRefund = tokenBalance[msg.sender];
-        require(senderRefund > 0, "You don't have tokens");
-        tokenBalance[msg.sender]=0;
-        ERC20Mintable(tokenAddress).mint(msg.sender, senderRefund);
+    function saveBoard(Game storage game, int8[9] memory board) private {
+        uint8 x = 0;
+
+        for (uint8 i = 0; i < 3; i++){
+            for (uint8 j = 0; j < 3; j++) {
+                int8 c = board[x];
+                require(c < 3 && c >= 0, "invalid mark in the boad");
+                game.board[i][j] = c;
+                x++;
+            }
+        }
     }
     
-    function claimRefund() public {
-        uint tokens = ERC20Mintable(tokenAddress).allowance(msg.sender, address(this));
-        require(tokens > 0, "You have to allow the contract to spend on your behalf");
-        ERC20Mintable(tokenAddress).transferFrom(msg.sender, address(this), tokens);
-        msg.sender.transfer(tokens);
+    function getBidDetails(uint id) public view returns(address, address, uint, BidStates, uint){
+        Bid memory currentBid = bids[id];
+        return (currentBid.bidder, currentBid.acceptor, currentBid.value, currentBid.state, currentBid.bidTimeOut);
     }
     
-    function getBoard(uint g) public view returns (uint8[9] memory board) {
+    function getGameDetails(uint id) public view returns(address, address, GameStates, uint8){
+        Game memory game = games[id];
+        return (game.X, game.O, game.state, game.numberOfTurns);
+    }
+    
+    function getBoard(uint g) public view returns (int8[9] memory board) {
         Game storage currentGame = games[g];
         uint8 x = 0;
 
@@ -184,4 +232,31 @@ contract TicTacToe {
         }
         return board;
     }
+    
+    function checkWinner(Game storage currentGame) private view returns (bool won, address winner){
+        uint8 i;
+        mapping(uint8 => mapping(uint8 => int8)) storage currentBoard = currentGame.board;
+        
+        for (i = 0; i<3; i++){
+            int8 rowSum = currentBoard[i][0] + currentBoard[i][1] + currentBoard[i][2];
+            int8 colSum = currentBoard[0][i] + currentBoard[1][i] + currentBoard[2][i];
+            if (rowSum == 3 || colSum == 3){
+                return (true, currentGame.X);
+            } else if (rowSum == -3 || colSum == -3){
+                return (true, currentGame.O);
+            }
+        }
+        
+        int8 dSum = currentBoard[0][0] + currentBoard[1][1] + currentBoard[2][2];
+        int8 rSum = currentBoard[2][0] + currentBoard[1][1] + currentBoard[0][2];
+        
+        if (dSum == 3 || rSum == 3){
+            return (true, currentGame.X);
+        } else if (dSum == -3 || rSum == -3){
+            return (true, currentGame.O);
+        }
+        
+        return (false, address(0));
+    }
+    
 }
